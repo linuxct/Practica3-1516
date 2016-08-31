df7f17408d53dc8522cafe9c77c3089a32105152
Sergio Castell <sergiocastell@users.noreply.github.com>
2016-08-31 18:54:31 +0200

    Limpieza del código
    Signed-off-by: Sergio Castell <sergiocastell@github.com>

--- p3-3.s
+++ p3-3.s
@@ -19,17 +19,17 @@ __start:
 	la	$v0,6					# preparamos entrada de numero en flotante a f0
 	syscall						# llamada al sistema
 	
-	mov.s	$f2,$f0 			# movemos f0 a f2 (radio)
-	mul.s	$f4,$f2,$f2			# multiplicamos f2 por si mismo, y guardamos el valor en f4 (f6 al cuadrado)
-	mul.s	$f4,$f4,$f2			# multiplicamos f4 (5 al cuadrado) por f2 (5), y guardamos el valor en f4 (f6 al cubo)
+	mov.s	$f2,$f0 				# movemos f0 a f2 (radio)
+	mul.s	$f4,$f2,$f2				# multiplicamos f2 por si mismo, y guardamos el valor en f4 (f6 al cuadrado)
+	mul.s	$f4,$f4,$f2				# multiplicamos f4 (5 al cuadrado) por f2 (5), y guardamos el valor en f4 (f6 al cubo)
 
-	mul.s	$f10,$f8,$f10		# multiplicamos 4 por pi y lo guardamos en f10
-	mul.s	$f10,$f4,$f10		# multiplicamos 4 por pi por r al cubo (f4) y lo guardamos en f10
-	div.s	$f10,$f10,$f6		# dividimos 4piRcubo entre 3
+	mul.s	$f10,$f8,$f10				# multiplicamos 4 por pi y lo guardamos en f10
+	mul.s	$f10,$f4,$f10				# multiplicamos 4 por pi por r al cubo (f4) y lo guardamos en f10
+	div.s	$f10,$f10,$f6				# dividimos 4piRcubo entre 3
 	la	$a0,str_2				# Mostramos str_3 por pantalla
 	li	$v0,4					# Preparamos salida por pantalla de cadena
 	syscall						# llamada al sistema
-	mov.s	$f12,$f10			# movemos el resultado f9 a f12 para mostrarlo por pantalla
+	mov.s	$f12,$f10				# movemos el resultado f9 a f12 para mostrarlo por pantalla
 	la		$v0,2				# preparamos salida por pantalla de un flotante
 	syscall						# llamada al sistema 
 	li $v0,10					# Carga 10 en V0 para acabar el programa
